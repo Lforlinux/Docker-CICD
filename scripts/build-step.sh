fb9601926f494adb9fcbc5646ec1f2f76c26c5b7
KLN <karthik.kln@gmail.com>
2020-09-20 13:58:18 +0530

    troubleshoot

--- scripts/build-step.sh
+++ scripts/build-step.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 echo "Build step"
-sudo docker build -t lforlinux/helloworldrepo .
+cat Dockerfile
+#sudo docker build -t lforlinux/helloworldrepo .
 pwd; ls -lrth
 echo "Pushing to registry"
 sudo docker push lforlinux/helloworldrepo
